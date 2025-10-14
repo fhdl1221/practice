c500f58b470d75d8bff238a4ba987ca113862191
fhdl1221 <fhdl353212@gmail.com>
2025-10-14 13:29:48 +0900

    쿼리 파라미터 실습 (#1)

--- react-router/src/pages/RootPages/Home.jsx
+++ react-router/src/pages/RootPages/Home.jsx
@@ -3,17 +3,15 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="flex gap-4 justify-center m-2">
-      <Link to="/dummy/products" className="m-2">
-        Products 페이지로 이동
+    <div className="flex justify-center gap-8 p-4 border-b-2 border-t-2 border-gray-800">
+      <Link to="/dummy/products" className="hover:text-yellow-400">
+        Products
       </Link>
-      <br />
-      <Link to="/dummy/carts" className="m-2">
-        Carts 페이지로 이동
+      <Link to="/dummy/carts" className="hover:text-yellow-400">
+        Carts
       </Link>
-      <br />
-      <Link to="/dummy/posts" className="m-2">
-        Posts 페이지로 이동
+      <Link to="/dummy/posts" className="hover:text-yellow-400">
+        Posts
       </Link>
     </div>
   );
